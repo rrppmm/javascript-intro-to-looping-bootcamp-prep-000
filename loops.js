397071749fe8d6c14b7267e0c5786a9ea76f6e4f
Paul M <paulmcadory@gmail.com>
2019-09-29 20:39:25 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,29 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+  if (i === 1){
+  console.log("I am 1 strange loop.");
+  }
+  else{
+  console.log("I am ${i} strange loops");
+  }
+  }
+  return array;
+}
+function whileLoop(number) {
+  while (number > 0) {
+    console.log(number--);
+  }
+  if (number === 0){
+    return 'done';
+  }
+  }
+  function doWhileLoop(number) {
+    function incrementVariable() {
+      number = number + 1;
+      return number;
+    }
+    do {
+      console.log("I run once regardless.");
+    } while 
+    (incrementVariable() < number);
+  }
