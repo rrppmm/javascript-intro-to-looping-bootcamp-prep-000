d878bbd36d752b5cc15ab9875edb83c27c384f00
Paul M <paulmcadory@gmail.com>
2019-09-29 20:47:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,7 @@ function forLoop(array) {
   array.push(`I am 1 strange loop.`);
   }
   else{
-  console.log(`I am ${i} strange loops.`);
+  array.push(`I am ${i} strange loops.`);
   }
   }
   return array;
